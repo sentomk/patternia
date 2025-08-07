0c6b675469e64cb8138b8b8e35e939b8fa3bc2cf
sentomk <sentomk040924@gmail.com>
2025-08-07 23:44:57 +0800

    refactor(match_builder): introduce private tag to hide ctor

--- include/ptn/patternia.hpp
+++ include/ptn/patternia.hpp
@@ -7,26 +7,26 @@
 
 namespace ptn {
 
-  // forward declare
-  template <typename T>
-  constexpr auto match(T &&) noexcept;
-
   /* class match_builder start */
   template <typename T, typename... Cases>
   class match_builder {
-    friend constexpr auto match(T &&) noexcept;
 
+    /* private tag type used to hide ctor */
+    struct ctor_tag {};
+
+    /* private member variables*/
   private:
     T                    value_;
     std::tuple<Cases...> cases_;
 
-    /* Perfect forwarding conductor. */
-    explicit constexpr match_builder(T v, std::tuple<Cases...> cs)
+    /* public member method */
+  public:
+    /* perfect forwarding ctor */
+    explicit constexpr match_builder(T v, std::tuple<Cases...> cs, ctor_tag = {})
         : value_(std::move(v)), cases_(cs) {
     }
 
-  public:
-    /* Return itself or a new builder. Only can be called on lvalue */
+    /* return itself or a new builder. Only can be called on lvalue */
     template <typename Pattern, typename Handler>
     constexpr auto with(Pattern p, Handler h) & {
       using pair_t   = std::pair<Pattern, Handler>;
@@ -34,7 +34,7 @@ namespace ptn {
       return match_builder<T, Cases..., pair_t>(std::move(value_), std::move(new_cases));
     }
 
-    /* Overload for "with", now it supports rvalue. */
+    /* overload for "with", now it supports rvalue. */
     template <typename Pattern, typename Handler>
     constexpr auto with(Pattern p, Handler h) && {
       using pair_t = std::pair<Pattern, Handler>;
@@ -71,6 +71,7 @@ namespace ptn {
     }
   }; // class match_builder
 
+  /* wrapper */
   template <typename T>
   constexpr auto match(T &&value) noexcept {
     using V = std::decay_t<T>;
