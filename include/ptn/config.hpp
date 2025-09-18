1a342ff78677dfa169f0f3b70a3be2e5d37f7227
sentomk <sentomk040924@gmail.com>
2025-09-18 19:07:39 +0800

    update: add c++17 support via SFINAE

--- include/ptn/config.hpp
+++ include/ptn/config.hpp
@@ -1,7 +1,15 @@
+// clang-format off
 #ifndef PTN_ENABLE_VALUE_PATTERN
-#define PTN_ENABLE_VALUE_PATTERN 1
+  #define PTN_ENABLE_VALUE_PATTERN 1
 #endif
 
 #ifndef PTN_ENABLE_RELATIONAL_PATTERN
-#define PTN_ENABLE_RELATIONAL_PATTERN 1
+  #define PTN_ENABLE_RELATIONAL_PATTERN 1
 #endif
+
+#if defined(__cpp_concepts) && __cpp_concepts >= 201907L
+  #define PTN_USE_CONCEPTS 1
+#else
+  #define PTN_USE_CONCEPTS 0
+#endif
+
