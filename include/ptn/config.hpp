50bbb733273c75776a0a08dc05cc3358223ea89e
sentomk <sentomk040924@gmail.com>
2025-10-20 16:43:45 +0800

    routine code review

--- include/ptn/config.hpp
+++ include/ptn/config.hpp
@@ -7,6 +7,10 @@
   #define PTN_ENABLE_RELATIONAL_PATTERN 1
 #endif
 
+#ifndef PTN_ENABLE_TYPE_PATTERN
+  #define PTN_ENABLE_TYPE_PATTERN 1
+#endif
+
 #if defined(__cpp_concepts) && __cpp_concepts >= 201907L
   #define PTN_USE_CONCEPTS 1
 #else
