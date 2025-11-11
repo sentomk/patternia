379841891cd681ffe40c8a70db2dd2ab0098ee2d
sentomk <sentomk040924@gmail.com>
2025-11-11 21:36:10 +0800

    chore(docs): restructure documentation and migrate to Markdown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,13 +15,20 @@ release = '0.4.2'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'breathe'
+    "myst_parser",      
+    "breathe",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.githubpages",
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -33,3 +40,16 @@ breathe_projects = {
     "patternia": "../build/doxygen/xml"
 }
 breathe_default_project = "patternia"
+
+html_title = "patternia"
+html_favicon = "_static/favicon.ico"
+
+# === Light / Dark logo ===
+html_theme_options = {
+    "light_logo": "logo-light.png",
+    "dark_logo": "logo-dark.png",
+    "sidebar_hide_name": True,
+    "navigation_with_keys": True,
+}
+
+html_css_files = ["custom.css"]
